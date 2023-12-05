12e652e70fc5b199661a1c314bd46a5068aca1eb
Tony K <tony-koke@outlook.com>
2023-12-05 15:26:24 +0000

    Basic HTML Elements added to page

---
+++ questions.js
@@ -0,0 +1,74 @@
+const tvShowPlotQuestions = [
+    {
+      question: "In the TV series 'Black Mirror,' what is the name of the virtual reality gaming system featured in the episode 'Striking Vipers'?",
+      options: {
+        a: "Infinity Game",
+        b: "Eternal Oasis",
+        c: "Striking Vipers X",
+        d: "Nebula Quest"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "In 'The Wire,' what is the name of the fictional city in which the series is primarily set?",
+      options: {
+        a: "Baltimore",
+        b: "Chicago",
+        c: "Detroit",
+        d: "Philadelphia"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "In 'Westworld,' what is the name of the corporation that created the hosts?",
+      options: {
+        a: "Delos Incorporated",
+        b: "HostTech Solutions",
+        c: "SynthCorp",
+        d: "TechWorld Innovations"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "In 'Breaking Bad,' what is the alias used by Gustavo Fring to conceal his true identity?",
+      options: {
+        a: "The Chemist",
+        b: "The Supplier",
+        c: "The Enigma",
+        d: "The Chicken Man"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What is the name of the mysterious town in 'Twin Peaks,' where the murder of Laura Palmer takes place?",
+      options: {
+        a: "Greendale",
+        b: "Riverdale",
+        c: "Twin Peaks",
+        d: "Silent Hill"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "In 'The Sopranos,' what is the nickname of Tony Soprano's therapist?",
+      options: {
+        a: "Dr. Dream",
+        b: "Dr. Melfi",
+        c: "Dr. Insight",
+        d: "Dr. Shrink"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "In 'Stranger Things,' what is the name of the government laboratory conducting experiments in Hawkins?",
+      options: {
+        a: "Project Sigma",
+        b: "Lab-X",
+        c: "Hawkins Research Facility",
+        d: "Hawkins National Laboratory"
+      },
+      correctAnswer: "d"
+    }
+];
+
+  
