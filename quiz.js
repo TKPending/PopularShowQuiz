eaccc1e4785b3b864f285e58469b6485e1f57189
Tony K <tony-koke@outlook.com>
2023-12-06 17:36:29 +0000

    Set up for quiz

--- quiz.js
+++ quiz.js
@@ -1,8 +1,19 @@
 // Create Submit Page
+let score = 0;
 
 const displayQuestion = (question) => {
-    console.log("Test")
-    document.getElementById('question').innerHTML = question
+    document.getElementById('question').textContent = question.question;
 }
 
-module.exports = {displayQuestion};
+const displayOptions = (options) => {
+    document.getElementById('option-one').textContent = options.a;
+    document.getElementById('option-two').textContent = options.b;
+    document.getElementById('option-three').textContent = options.c;
+    document.getElementById('option-four').textContent = options.d;
+}
+
+const correctAnswer = (userInput, questionAnswer) => {
+    if (userInput == questionAnswer) {
+        score++;
+    }
+}
