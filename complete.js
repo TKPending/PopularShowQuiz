c18227a8a3a402c0c33b5810353f727c356fda79
Tony K <tony-koke@outlook.com>
2023-12-08 09:26:47 +0000

    Updated completed section

--- complete.js
+++ complete.js
@@ -1,31 +1,28 @@
-const completeDisplay = {
-    "red": {
-        message: '<h1 class="x-auto text-4xl text-center transition duration-500">:/<br>Are you sure you watched these shows before?</h1>',
-        bgColor: 'red'
-    }, 
-    "orange": {
-        message: '<h1 class="x-auto text-4xl text-center transition duration-500">:(<br>Not bad. Haven\'t watched these shows in a while?</h1>',
-        bgColor: 'orange'
-    },
-    "green": {
-        message: '<h1 class="x-auto text-4xl text-center transition duration-500">:)<br>TV Fanatic. Recommend some TV shows to me</h1>',
-        bgColor: 'green'
+const completeDisplayText = (finalScore) => {
+    if (finalScore < 3) {
+        return {
+            message: `<h1 class="x-auto text-4xl text-center transition duration-500">${finalScore} / 7<br> :/<br>Are you sure you watched these shows before?</h1>`,
+            bgColor: 'red'
+        };
+    } else if (finalScore >= 3 && finalScore < 5) {
+        return {
+            message: `<h1 class="x-auto text-4xl text-center transition duration-500">${finalScore} / 7<br>:(<br>Not bad. Haven't watched these shows in a while?</h1>`,
+            bgColor: 'orange'
+        };
+    } else {
+        return {
+            message: `<h1 class="x-auto text-4xl text-center transition duration-500">${finalScore} / 7<br>:)<br>TV Fanatic. Recommend some TV shows to me</h1>`,
+            bgColor: 'green'
+        };
     }
- }
+};
+
 
 
 export const quizCompletion = (finalScore) => {
-    let resultDisplay;
-    
-    if (finalScore < 3) {
-        resultDisplay = completeDisplay.red;
-    } else if (finalScore >= 3 && finalScore < 5) {
-        resultDisplay = completeDisplay.orange;
-    } else {
-        resultDisplay = completeDisplay.green;
-    }
+    const completeDisplay = completeDisplayText(finalScore);
 
     // Display the result message and set the background color
-    document.body.innerHTML = resultDisplay.message;
-    document.body.style.backgroundColor = resultDisplay.bgColor;
+    document.body.innerHTML = completeDisplay.message;
+    document.body.style.backgroundColor = completeDisplay.bgColor;
 };
